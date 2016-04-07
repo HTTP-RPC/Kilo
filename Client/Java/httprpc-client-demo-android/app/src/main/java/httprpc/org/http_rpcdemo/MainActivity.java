c3e4dbd345d9652c827ff4853ad998bc94fcf1f7
Greg Brown <gk_brown@verizon.net>
2016-04-07 09:27:24 -0400

    Stub out Android client demo.

---
+++ Client/Java/httprpc-client-demo-android/app/src/main/java/httprpc/org/http_rpcdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package httprpc.org.http_rpcdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
