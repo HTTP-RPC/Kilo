1dfb4186698f348b8e8c65ea563f8091acf9bad9
Greg Brown <gk_brown@verizon.net>
2016-12-02 10:49:00 -0500

    Add iOS source files.

--- Client/Objective-C/HTTPRPC-iOS/HTTPRPC/HTTPRPC.h
+++ Client/Objective-C/HTTPRPC-iOS/HTTPRPC/HTTPRPC.h
@@ -1,12 +1,18 @@
 //
-//  HTTPRPC.h
-//  HTTPRPC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Greg Brown on 12/2/16.
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for HTTPRPC.
 FOUNDATION_EXPORT double HTTPRPCVersionNumber;
@@ -14,6 +20,4 @@ FOUNDATION_EXPORT double HTTPRPCVersionNumber;
 //! Project version string for HTTPRPC.
 FOUNDATION_EXPORT const unsigned char HTTPRPCVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <HTTPRPC/PublicHeader.h>
-
-
+#import <HTTPRPC/WSWebServiceProxy.h>
