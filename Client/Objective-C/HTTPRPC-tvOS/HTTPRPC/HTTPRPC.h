0528dde977d35a243cb9c274d58a4c729427ddcd
Greg Brown <gk_brown@verizon.net>
2016-12-02 10:46:20 -0500

    Stub out framework projects.

---
+++ Client/Objective-C/HTTPRPC-tvOS/HTTPRPC/HTTPRPC.h
@@ -0,0 +1,19 @@
+//
+//  HTTPRPC.h
+//  HTTPRPC
+//
+//  Created by Greg Brown on 12/2/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPRPC.
+FOUNDATION_EXPORT double HTTPRPCVersionNumber;
+
+//! Project version string for HTTPRPC.
+FOUNDATION_EXPORT const unsigned char HTTPRPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPRPC/PublicHeader.h>
+
+
