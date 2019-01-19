90995c240e141c0484af06e7b5b7a5e836d59229
Greg Brown <gk_brown@icloud.com>
2019-01-19 10:47:01 -0500

    Update flat file encoder test.

--- httprpc/src/test/java/org/httprpc/io/FlatFileEncoderTest.java
+++ httprpc/src/test/java/org/httprpc/io/FlatFileEncoderTest.java
@@ -21,32 +21,31 @@ import java.util.Map;
 
 import org.httprpc.AbstractTest;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class FlatFileEncoderTest extends AbstractTest {
     @Test
-    @Ignore // TODO
     public void testWrite() throws IOException {
-        String expected = ""; // TODO
+        String expected = "     AB C 000012.35\r\n" +
+            "  DEFGH IJ000024.57\r\n";
 
         List<Map<String, ?>> values = listOf(
             mapOf(
                 entry("a", " AB C "),
                 entry("b", 1),
-                entry("c", 2.0)
+                entry("c", 2.345)
             ),
             mapOf(
                 entry("a", "  DEFGH IJKL "),
                 entry("b", 2),
-                entry("c", 4.0)
+                entry("c", 4.5678)
             )
         );
 
         StringWriter writer = new StringWriter();
 
         FlatFileEncoder flatFileEncoder = new FlatFileEncoder(listOf(
-            new FlatFileEncoder.Field("a", "%10s"),
+            new FlatFileEncoder.Field("a", "%10.10s"),
             new FlatFileEncoder.Field("b", "%05d"),
             new FlatFileEncoder.Field("c", "%04.2f")
         ));
