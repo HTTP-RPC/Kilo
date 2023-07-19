d8a055de453247deae73eb2b1a8c1a9ab73f0b28
Greg Brown <gk_brown@icloud.com>
2023-07-19 11:07:17 -0400

    Add XML transform example.

---
+++ kilo-test/src/test/java/org/httprpc/kilo/XMLTransformTest.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.httprpc.kilo;
+
+import org.httprpc.kilo.io.TemplateEncoder;
+import org.httprpc.kilo.xml.ElementAdapter;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class XMLTransformTest {
+    public static void main(String[] args) {
+        try {
+            var xmlTransformTest = new XMLTransformTest();
+
+            var t0 = System.currentTimeMillis();
+
+            xmlTransformTest.transformXML1();
+
+            var t1 = System.currentTimeMillis();
+
+            System.out.printf("%s %dms\n", Transformer.class.getSimpleName(), (t1 - t0));
+
+            xmlTransformTest.transformXML2();
+
+            var t2 = System.currentTimeMillis();
+
+            System.out.printf("%s %dms\n", TemplateEncoder.class.getSimpleName(), (t2 - t1));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void transformXML1() throws Exception {
+        var source = new StreamSource(getClass().getResourceAsStream("breakfast_menu.xslt"));
+
+        var transformer = TransformerFactory.newInstance().newTransformer(source);
+
+        var xmlSource = new StreamSource(getClass().getResourceAsStream("breakfast_menu.xml"));
+
+        var homeDirectory = new File(System.getProperty("user.home"));
+        var outputFile = new File(homeDirectory, "breakfast_menu_1.html");
+
+        try (var outputStream = new FileOutputStream(outputFile)) {
+            transformer.transform(xmlSource, new StreamResult(outputStream));
+        }
+    }
+
+    private void transformXML2() throws Exception {
+        var documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+        Document document;
+        try (var inputStream = getClass().getResourceAsStream("breakfast_menu.xml")) {
+            document = documentBuilder.parse(inputStream);
+        }
+
+        var templateEncoder = new TemplateEncoder(getClass().getResource("breakfast_menu.html"));
+
+        var homeDirectory = new File(System.getProperty("user.home"));
+        var outputFile = new File(homeDirectory, "breakfast_menu_2.html");
+
+        try (var outputStream = new FileOutputStream(outputFile)) {
+            templateEncoder.write(new ElementAdapter(document.getDocumentElement()), outputStream);
+        }
+    }
+}
