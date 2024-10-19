bc87712f706496f96a045fad150af99d95aac500
Greg Brown <gk_brown@icloud.com>
2024-10-19 16:23:08 -0400

    Restore XML transform example.

---
+++ kilo-test/src/test/java/org/httprpc/kilo/test/XMLTransformTest.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.httprpc.kilo.test;
+
+import org.httprpc.kilo.io.TemplateEncoder;
+import org.httprpc.kilo.xml.ElementAdapter;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class XMLTransformTest {
+    public static void main(String[] args) throws Exception {
+        var xmlTransformTest = new XMLTransformTest();
+
+        var t0 = System.currentTimeMillis();
+
+        xmlTransformTest.transformXML1();
+
+        var t1 = System.currentTimeMillis();
+
+        System.out.printf("%s %dms\n", Transformer.class.getSimpleName(), (t1 - t0));
+
+        xmlTransformTest.transformXML2();
+
+        var t2 = System.currentTimeMillis();
+
+        System.out.printf("%s %dms\n", TemplateEncoder.class.getSimpleName(), (t2 - t1));
+    }
+
+    private void transformXML1() throws Exception {
+        var source = new StreamSource(getClass().getResourceAsStream("breakfast-menu.xslt"));
+
+        var transformer = TransformerFactory.newInstance().newTransformer(source);
+
+        var xmlSource = new StreamSource(getClass().getResourceAsStream("breakfast-menu.xml"));
+
+        var outputFile = Paths.get(System.getProperty("user.home"), "breakfast-menu-1.html");
+
+        try (var outputStream = Files.newOutputStream(outputFile)) {
+            transformer.transform(xmlSource, new StreamResult(outputStream));
+        }
+    }
+
+    private void transformXML2() throws Exception {
+        var documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+        documentBuilderFactory.setExpandEntityReferences(false);
+        documentBuilderFactory.setIgnoringComments(true);
+
+        var documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+        Document document;
+        try (var inputStream = getClass().getResourceAsStream("breakfast-menu.xml")) {
+            document = documentBuilder.parse(inputStream);
+        }
+
+        var templateEncoder = new TemplateEncoder(getClass(), "breakfast-menu.html");
+
+        var outputFile = Paths.get(System.getProperty("user.home"), "breakfast-menu-2.html");
+
+        try (var outputStream = Files.newOutputStream(outputFile)) {
+            templateEncoder.write(new ElementAdapter(document.getDocumentElement()), outputStream);
+        }
+    }
+}
---
+++ kilo-test/src/test/resources/org/httprpc/kilo/test/breakfast-menu.xslt
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<html xsl:version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <body style="font-family:Arial;font-size:12pt;background-color:#EEEEEE">
+        <xsl:for-each select="breakfast_menu/food">
+            <div style="background-color:teal;color:white;padding:4px">
+                <span style="font-weight:bold"><xsl:value-of select="name"/> - </span>
+                <xsl:value-of select="price"/>
+            </div>
+            <div style="margin-left:20px;margin-bottom:1em;font-size:10pt">
+                <p>
+                    <xsl:value-of select="description"/>
+                    <span style="font-style:italic"> (<xsl:value-of select="calories"/> calories per serving)</span>
+                </p>
+            </div>
+        </xsl:for-each>
+    </body>
+</html>
